5d9cec9e919ba789d7adde676c4de8c6196457f8
olynka <98587532+olynka@users.noreply.github.com>
2022-11-02 19:47:29 +0200

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-import { Component } from 'react';
-
+import { useState } from 'react';
 import { Notification } from './Notification/Notification';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
@@ -7,49 +6,56 @@ import { Feedback } from './feedback/feedback';
 
 
 
-export class App extends Component  {
-state = {
- good: 0,
-  neutral: 0,
-  bad: 0
-  }
-  countTotalFeedback=()=> {
-    const {neutral, good, bad} = this.state
+
+
+export function App() { 
+  const [good, setGood] =useState(0);
+  const [neutral,setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+
+    const countTotalFeedback=()=> {
     return neutral + good + bad
   }
 
-  countPositiveFeedbackPercentage() {
-    const {neutral, good, bad} = this.state
-    const total = neutral + good + bad
-     return Math.round((good / total) * 100);
-  }
+   const  countPositiveFeedbackPercentage = good => {
+     const total = countTotalFeedback();
+    if (!total) {
+      return 0;
+    }
+    return Math.round((good / total) * 100);
+  };
+  
+  const IncrementStatistics = btnName => {
+    switch (btnName) {
+      case 'good':
+        return setGood(prev => prev + 1);
 
+      case 'neutral':
+        return setNeutral(prev => prev + 1);
 
-
-  IncrementStatistics = propertyName => {
-    this.setState(prevState => {
-      const value = prevState[propertyName];
-      return {
-        [propertyName]: value + 1,
-      };
-    });
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        return;
+    }
   };
 
+    const total = countTotalFeedback();
+    const goodPercent = countPositiveFeedbackPercentage(good);
+   
 
-
-  render() {
- const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const goodPercent = this.countPositiveFeedbackPercentage();
-    const neutralPercent = this.countPositiveFeedbackPercentage();
+  
   return (
+  
   <>
     <Section title="Please leave feedback">
-        <Feedback IncrementStatistics={this.IncrementStatistics} />
+        <Feedback IncrementStatistics={IncrementStatistics} />
        </Section>
       <Section title="Statistics">
        
-  {this.countTotalFeedback() === 0 ? (
+  {countTotalFeedback() === 0 ? (
             <Notification />
           ) : (
           <Statistics
@@ -58,7 +64,7 @@ state = {
   bad={bad}
   total={total}
   goodPercent={goodPercent}
-  neutralPercent={neutralPercent}
+  neutralPercent={countPositiveFeedbackPercentage}
           
           />
           )}
@@ -66,4 +72,3 @@ state = {
      </>
   )
 }
-};
